5f04eba3809f7d3c2e64c3f023c43ca1fd6eb553
Helena Pedro <mbeua94@gmail.com>
2026-01-15 14:43:42 -0600

    Initial setup

---
+++ feedback/resume-api/src/test/java/com/pedro/resumeapi/ResumeApiApplicationTests.java
@@ -0,0 +1,15 @@
+package com.pedro.resumeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@SpringBootTest
+class ResumeApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ feedback/resume-worker/src/main/java/com/pedro/resumeworker/ResumeWorkerApplication.java
@@ -0,0 +1,13 @@
+package com.pedro.resumeworker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumeWorkerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResumeWorkerApplication.class, args);
+    }
+
+}
