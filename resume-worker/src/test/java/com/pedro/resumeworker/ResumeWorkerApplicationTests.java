4797549d230bb65a4716fb22f253320f2a5273a0
Helena Pedro <mbeua94@gmail.com>
2026-01-15 14:43:42 -0600

    Initial setup

---
+++ resume-worker/src/test/java/com/pedro/resumeworker/ResumeWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedro.resumeworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeWorkerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
