54db0f235dc60296fc3bf5df30a7854ff9d1a88d
Helena Pedro <mbeua94@gmail.com>
2026-01-23 01:49:08 -0600

    new commit

--- resume-api/src/main/java/com/pedro/resumeapi/ai/kafka/AiJobKafkaConfig.java
+++ resume-api/src/main/java/com/pedro/resumeapi/ai/kafka/AiJobKafkaConfig.java
@@ -3,13 +3,13 @@ package com.pedro.resumeapi.ai.kafka;
 import com.pedro.common.ai.AiJobRequestedMessage;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
-import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.support.serializer.JsonSerializer;
+
 
 import java.util.HashMap;
 import java.util.Map;
