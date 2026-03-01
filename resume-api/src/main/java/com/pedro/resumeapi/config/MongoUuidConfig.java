4e2bba53f027ae20f8edf944aa1222368b67147c
Helena Pedro <mbeua94@gmail.com>
2026-02-28 22:10:28 -0600

    nc

---
+++ resume-api/src/main/java/com/pedro/resumeapi/config/MongoUuidConfig.java
@@ -0,0 +1,23 @@
+package com.pedro.resumeapi.config;
+
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.bson.UuidRepresentation;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MongoUuidConfig {
+
+    @Bean
+    MongoClient mongoClient(@Value("${spring.data.mongodb.uri}") String mongoUri) {
+        MongoClientSettings settings = MongoClientSettings.builder()
+                .applyConnectionString(new ConnectionString(mongoUri))
+                .uuidRepresentation(UuidRepresentation.STANDARD)
+                .build();
+        return MongoClients.create(settings);
+    }
+}
